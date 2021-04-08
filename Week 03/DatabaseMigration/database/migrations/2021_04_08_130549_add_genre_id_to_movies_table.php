7a0511da77b058b7bbcdb4d20c6ee2d89da8984e
lexyhandi <lexihandi@gmail.com>
2021-04-08 20:48:31 +0700

    update migration

---
+++ Week 03/DatabaseMigration/database/migrations/2021_04_08_130549_add_genre_id_to_movies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddGenreIdToMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            $table->unsignedBigInteger('genre_id');
+            $table->foreign('genre_id')
+                ->references('id')->on('movies')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            $table->dropForeign('genre_id');
+            $table->dropColumn('genre_id');
+        });
+    }
+}
